e2b4840c02dc412e169120a21a53594a66ff1bf0
APURI12 <84340739+APURI12@users.noreply.github.com>
2026-09-08 15:38:47 -0300

    some functionality

--- Wordle.py
+++ Wordle.py
@@ -1,2 +1,14 @@
 from wonderwords import RandomWord
 
+w=RandomWord
+word=w.word(word_min_length=5,word_max_length=5)
+
+i=0
+while i<6:
+    k=''
+    while len(k)!=5:
+        k=input("Input a valid 5 letter word: ")
+    for j in range(0,5):
+        if k[j] in word:
+            print(k[j]+" is in the word.")
+    i+=1
