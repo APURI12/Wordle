2f067a6143e468170f8d8c7d688467501b32a76f
APURI12 <84340739+APURI12@users.noreply.github.com>
2026-09-08 16:24:50 -0300

    Core functioning game with minor bugs

--- Wordle.py
+++ Wordle.py
@@ -1,14 +1,23 @@
 from wonderwords import RandomWord
 
-w=RandomWord
-word=w.word(word_min_length=5,word_max_length=5)
-
-i=0
-while i<6:
+w=RandomWord()
+word=w.word(word_min_length=5,word_max_length=5).strip().lower()
+i=1
+while i<7:
     k=''
     while len(k)!=5:
-        k=input("Input a valid 5 letter word: ")
+        k=input("Input a valid 5 letter word: ").strip().lower()
+    output=""
+    if word==k:
+        print("You guessed the word in "+ str(i) +" tries!")
+        break
     for j in range(0,5):
-        if k[j] in word:
-            print(k[j]+" is in the word.")
+        if k[j] == word[j]:
+            output+=k[j]+" is green. "
+        elif k[j] in word[j]:
+            output+=k[j]+" is yellow. "
+        else:
+            output+=k[j]+" is grey. "
+    print(output)
+    output=''
     i+=1
